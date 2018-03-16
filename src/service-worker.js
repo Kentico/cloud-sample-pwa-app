afb7b866f3797a89d6643d600370f5573692745b
Simply007 <chrastina.ondra@gmail.com>
2018-03-16 09:05:22 +0100

    add icons for cross browser

---
+++ src/service-worker.js
@@ -0,0 +1,27 @@
+var dataCacheName = 'travelApp-v1';
+var cacheName = 'travelAppData-v1';
+
+var filesToCache = [
+    '/',
+    '/index.html',
+    '/scripts/app.js',
+    '/styles/style.css',
+    '/styles/fonts/RobotoMedium.eot',
+    '/styles/fonts/RobotoMedium.svg',
+    '/styles/fonts/RobotoMedium.ttf',
+    '/styles/fonts/RobotoMedium.woff',
+    '/styles/fonts/RobotoMedium.woff2',
+    '/assets/images/icon_refresh.svg',
+    '/images/cloudy-scattered-showers.png',
+    '/images/cloudy.png',
+    '/images/fog.png',
+    '/images/ic_add_white_24px.svg',
+    '/images/ic_refresh_white_24px.svg',
+    '/images/partly-cloudy.png',
+    '/images/rain.png',
+    '/images/scattered-showers.png',
+    '/images/sleet.png',
+    '/images/snow.png',
+    '/images/thunderstorm.png',
+    '/images/wind.png'
+  ];
