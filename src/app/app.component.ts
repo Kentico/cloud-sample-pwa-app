54382f79799067dd532fadb41e76acee634b256f
Simply007 <chrastina.ondra@gmail.com>
2018-04-06 14:22:44 +0200

    Add offline sample

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  showOfflineIcon: boolean;
+
+  ngOnInit(): void {
+    this.showOfflineIcon = !navigator.onLine;
+    window.addEventListener('online', () => this.showOfflineIcon = false);
+    window.addEventListener('offline', () => this.showOfflineIcon = true);
+  }
 }
